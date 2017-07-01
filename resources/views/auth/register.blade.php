7905066b5ee1ec175faee4d4a6b47d2866493f00
= <=>
2017-07-01 11:50:21 +0100

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,25 +32,13 @@
                     </form>
                     <form method='POST' action='https://voguepay.com/pay/'>
                    <input type='hidden' name='v_merchant_id' value='3704-0052968' />
-<input type='hidden' name='merchant_ref' value='234-567-890' />
-<input type='hidden' name='memo' value='Bulk order from McAckney Web Shop' />
+<input type='hidden' name='memo' value='July Flash Sales Registration Fee' />
 
-<input type='hidden' name='item_1' value='Face Cap' />
-<input type='hidden' name='description_1' value='Blue Zizi facecap' />
-<input type='hidden' name='price_1' value='2000' />
+<input type='hidden' name='item_1' value='Registration' />
+<input type='hidden' name='description_1' value='Registration Fee' />
+<input type='hidden' name='price_1' value='10000' />
 
-<input type='hidden' name='item_2' value='Laban T-shirt' />
-<input type='hidden' name='description_2' value='Green XXL' />
-<input type='hidden' name='price_2' value='3000' />
-
-<input type='hidden' name='item_3' value='Black Noni Shoe' />
-<input type='hidden' name='description_3' value='Size 42' />
-<input type='hidden' name='price_3' value='8000' />
-
-<input type='hidden' name='developer_code' value='pq7778ehh9YbZ' />
-<input type='hidden' name='store_id' value='25' />
-
-<input type='hidden' name='total' value='13000' />
+<input type='hidden' name='total' value='10000' />
 
 <input type='image' src='http://voguepay.com/images/buttons/buynow_blue.png' alt='Submit' />
 
